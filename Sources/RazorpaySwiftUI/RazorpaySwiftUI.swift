4470a8c93e3eb36ddc580594fcd650d3aa46490e
hritikutekar <hritikdrocks@gmail.com>
2023-02-02 12:58:04 +0530

    chore: remove setRazorpayKey method

--- Sources/RazorpaySwiftUI/RazorpaySwiftUI.swift
+++ Sources/RazorpaySwiftUI/RazorpaySwiftUI.swift
@@ -11,10 +11,4 @@ public class RazorpaySwiftUI {
     public var razorpayKey: String = ""
     public var contact: String = ""
     public var email: String = ""
-    
-    public init() {}
-    
-    public func setRazorpayKey(_ key: String) {
-        self.razorpayKey = key
-    }
 }
