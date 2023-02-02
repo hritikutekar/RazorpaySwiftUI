29a69062d3352a2f77644fb11fa147e1bf8776c0
Hritik Utekar <hritikdrocks@gmail.com>
2023-02-02 11:25:43 +0530

    chore: set iOS 13 as platform

--- Package.swift
+++ Package.swift
@@ -5,11 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "RazorpaySwiftUI",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RazorpaySwiftUI",
-            targets: ["RazorpaySwiftUI", "Razorpay"]),
+            targets: ["RazorpaySwiftUI", "Razorpay"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,13 +22,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RazorpaySwiftUI",
-            dependencies: []),
+            dependencies: []
+        ),
         .binaryTarget(
             name: "Razorpay",
             path: "Razorpay.xcframework"
         ),
         .testTarget(
             name: "RazorpaySwiftUITests",
-            dependencies: ["RazorpaySwiftUI"]),
+            dependencies: ["RazorpaySwiftUI"]
+        ),
     ]
 )
