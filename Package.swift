18d4a0bfdbd4a252c08faeb1f1bdc742d7a568ba
Hritik Utekar <hritikdrocks@gmail.com>
2023-02-02 10:05:14 +0530

    chore

--- Package.swift
+++ Package.swift
@@ -9,12 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RazorpaySwiftUI",
-            targets: ["RazorpaySwiftUI"]),
+            targets: ["RazorpaySwiftUI", "Razorpay"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "Razorpay.xcframework")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,6 +21,10 @@ let package = Package(
         .target(
             name: "RazorpaySwiftUI",
             dependencies: []),
+        .binaryTarget(
+            name: "Razorpay",
+            path: "Razorpay.xcframework"
+        ),
         .testTarget(
             name: "RazorpaySwiftUITests",
             dependencies: ["RazorpaySwiftUI"]),
